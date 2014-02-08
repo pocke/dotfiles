cbb87ba9bd2c0f81ba604b16171656d68dcfd44b
pocke <p.ck.t22@gmail.com>
2014-02-08 21:55:56 +0900

    Window の透過

--- rc.lua
+++ rc.lua
@@ -434,8 +434,14 @@ client.connect_signal("manage", function (c, startup)
     end
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c)
+  c.border_color = beautiful.border_focus
+  c.opacity = 1
+  end)
+client.connect_signal("unfocus", function(c)
+  c.border_color = beautiful.border_normal
+  c.opacity = 0.8
+  end)
 -- }}}
 
 -- {{{ applet
