785dfd2728aa53a0a8773654a0c1206b4dfd290e
pocke <p.ck.t22@gmail.com>
2014-02-09 13:01:20 +0900

    (rc.lua) layouts の順番変えた

--- rc.lua
+++ rc.lua
@@ -55,7 +55,6 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.floating,
 --    awful.layout.suit.tile,
 --    awful.layout.suit.tile.left,
 --    awful.layout.suit.tile.bottom,
@@ -67,6 +66,7 @@ local layouts =
     awful.layout.suit.max,
 --    awful.layout.suit.max.fullscreen,
 --    awful.layout.suit.magnifier
+    awful.layout.suit.floating,
 }
 -- }}}
 
