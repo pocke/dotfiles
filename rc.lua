ac561462d606135302706d3920eb567dc4e35ef4
pocke <p.ck.t22@gmail.com>
2014-05-07 21:46:01 +0900

    (awesome) screen lock keybind

--- rc.lua
+++ rc.lua
@@ -246,6 +246,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey,           }, "q", function () awful.util.spawn("xtrlock -b") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
