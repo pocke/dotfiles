6c1aebc1deb02ee39a58e652363922a174ebe917
pocke <p.ck.t22@gmail.com>
2014-07-05 16:33:14 +0900

    (awesome) add layout

--- rc.lua
+++ rc.lua
@@ -62,7 +62,7 @@ local layouts =
 --    awful.layout.suit.tile.bottom,
 --    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
---    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.fair.horizontal,
 --    awful.layout.suit.spiral,
 --    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
