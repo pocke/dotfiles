fbe68820c2760bad5a249e66f65bb7d5a0a3fea6
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-10-23 15:16:31 +0900

    (awesome) Add theme

--- install.rb
+++ install.rb
@@ -16,7 +16,7 @@ FileUtils.mkdir_p(File.expand_path('~/.config/peco/'))
 %w[
   .gemrc .rspec .tigrc .vimperatorrc .xinitrc .yaourtrc
   .tmux.conf .vimrc .Xmodmap .ctags .gvimrc
-  .config/awesome/rc.lua .config/fontconfig/fonts.conf .config/peco/config.json
+  .config/awesome/rc.lua .config/awesome/themes .config/fontconfig/fonts.conf .config/peco/config.json
   .bundle/config
   .vim/after
   .vim/colors
