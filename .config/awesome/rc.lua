cfc4e959197e258ff514d511639cd2997e328620
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-03-23 22:08:17 +0900

    (awesome) not change opacity

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -520,10 +520,8 @@ end)
 
 client.connect_signal("focus", function(c)
   c.border_color = beautiful.border_focus
-  c.opacity = 1
   end)
 client.connect_signal("unfocus", function(c)
   c.border_color = beautiful.border_normal
-  c.opacity = 0.8
   end)
 -- }}}
