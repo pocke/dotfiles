8c45a4ee8b5eb515059fed9b80839638957ea325
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-16 18:11:00 +0900

    (install.rb) Fix broken installation

--- install.rb
+++ install.rb
@@ -27,7 +27,9 @@ FileUtils.mkdir_p(File.expand_path('~/.config/peco/'))
   end
 end
 
-exec "git clone https://github.com/rhenium/awesome-battmon.git ~/.config/awesome/plugins/battmon"
+unless File.exist?(File.expand_path("~/.config/awesome/plugins/battmon"))
+  exec "git clone https://github.com/rhenium/awesome-battmon.git ~/.config/awesome/plugins/battmon"
+end
 
 is_arch = File.exist?('/etc/pacman.conf')
 exec('sudo pacman -S words nodejs npm go tmux tig; true') if is_arch
