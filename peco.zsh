752620171ed695cd63817fb523629ac6008e1a1b
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-12-18 13:53:06 +0900

    (zsh) Add peco integration for select file(s)

--- peco.zsh
+++ peco.zsh
@@ -38,3 +38,15 @@ function peco-ghq-move()
 }
 zle -N peco-ghq-move
 bindkey '^G' peco-ghq-move
+
+function peco-select-file()
+{
+  local selected_file="$(git ls-files | peco --initial-filter=Fuzzy)"
+  if [ -n "$selected_file" ]; then
+    RBUFFER=$selected_file
+    CURSOR=$#BUFFER
+  fi
+  zle clear-screen
+}
+zle -N peco-select-file
+bindkey '^F' peco-select-file
