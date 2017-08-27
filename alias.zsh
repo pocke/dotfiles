64051d5dbaf671175c29c897c2b3d8fa19c4f6fa
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-08-27 17:18:04 +0900

    (zsh) Remove ALL alias

--- alias.zsh
+++ alias.zsh
@@ -26,7 +26,6 @@ if which xsel > /dev/null 2>&1; then
   alias -g C='| xsel --input --clipboard'
 fi
 alias -g N='> /dev/null 2>&1'
-alias -g ALL='**/*~.git/*~*/.git/*(.)'
 alias -g V='| vim -c "set buftype=nofile" - '
 # http://qiita.com/Kuniwak/items/b711d6c3e402dfd9356b
 alias -g B='`git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"`'
