246da74cf9eb44f599174aa76889c93342170dd3
Masataka Pocke Kuwabara <pocke@cookpad.com>
2018-04-16 09:40:24 +0900

    For macOS

--- install.rb
+++ install.rb
@@ -22,7 +22,6 @@ FileUtils.mkdir_p(File.expand_path('~/.config/peco/'))
   .vim/after
   .vim/colors
   .vim/spell
-  .vim/syntax
 ].each do |file|
   unless File.exist?(File.expand_path("~/#{file}"))
     exec("ln -s ~/dotfiles/#{file} ~/#{file}")
@@ -60,8 +59,5 @@ unless Dir.exist?(File.expand_path('~/.zsh/zsh-syntax-highlighting'))
   exec('git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting')
 end
 
-exec('vim +NeoBundleInstall +q')
-exec('sudo npm install -g jshint bower jsonlint typescript typescript-tools tslint')
 exec('gem install bundler')
-exec('bundle install')
 exec('go get github.com/motemen/ghq github.com/jteeuwen/go-bindata/... github.com/pocke/www github.com/golang/lint/golint github.com/peco/peco/...  github.com/pocke/www github.com/pocke/zettai github.com/alecthomas/gometalinter')
