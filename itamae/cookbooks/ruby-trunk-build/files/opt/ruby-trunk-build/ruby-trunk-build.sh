cc6225c1f0756b5e3f8153670d3c85b3cec55e7c
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-01-20 23:24:22 +0900

    (itamae) Retry fetching ruby/ruby to wait network up

--- itamae/cookbooks/ruby-trunk-build/files/opt/ruby-trunk-build/ruby-trunk-build.sh
+++ itamae/cookbooks/ruby-trunk-build/files/opt/ruby-trunk-build/ruby-trunk-build.sh
@@ -4,12 +4,24 @@ set -xe
 
 SRC='/tmp/ruby-trunk-build-src'
 
+function retry() {
+  local i
+  local st
+  for i in {1..5}; do
+    "$@"
+    st=$?
+    test $st -eq 0 && break
+    sleep $i
+  done
+  return $st
+}
+
 if [ ! -d $SRC ]; then
-  git clone --depth 1 https://github.com/ruby/ruby $SRC
+  retry git clone --depth 1 https://github.com/ruby/ruby $SRC
   cd $SRC
 else
   cd $SRC
-  git fetch
+  retry git fetch
   git checkout origin/trunk
 fi
 
